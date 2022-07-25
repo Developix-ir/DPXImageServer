2d67c024eaf31912ca47b8c207cdd4f5982f6a01
Erfan Mola <hi@erfanxd.ir>
2022-07-25 03:18:23 +0000

    Update DPXImageServer.php

--- DPXImageServer.php
+++ DPXImageServer.php
@@ -2,25 +2,25 @@
 /*
     Author: Erfan Mola
     https://Developix.ir
-    Version: 1.0.0
+    Version: 1.0.1
 */
 
 defined('DPXImageServer_Storage') or define('DPXImageServer_Storage', __DIR__ . "/DPXImageServerStorage");
 defined('DPXImageServer_TTL') or define('DPXImageServer_TTL', 365 * 86400);
 defined('DPXImageServer_PNGQuant') or define('DPXImageServer_PNGQuant', "pngquant");
 
-function DPXServeImage(string $file_path, int $width = null, int $height = null, int $quality = 80, bool $output_image = true, bool $cache = true)
-{
+function DPXServeImage(string $file_path, int $width = null, int $height = null, int $quality = 80, bool $output_image = true, bool $cache = true) {
 
     if (file_exists($file_path)) {
 
         $mime = strtolower(mime_content_type($file_path));
 
-        if (in_array($mime, ['image/png', 'image/jpg', 'image/jpeg'])) {
+        if (in_array($mime, [ 'image/png', 'image/jpg', 'image/jpeg' ])) {
 
             if (!(is_dir(DPXImageServer_Storage))) {
 
                 @mkdir(DPXImageServer_Storage);
+    
             }
 
             $image = null;
@@ -32,22 +32,27 @@ function DPXServeImage(string $file_path, int $width = null, int $height = null,
                 if (file_exists(DPXImageServer_Storage . "/$filename") && ((int)filemtime(DPXImageServer_Storage . "/$filename") > (time() - DPXImageServer_TTL))) {
 
                     $image = file_get_contents(DPXImageServer_Storage . "/$filename");
+
                 }
+
             }
 
             if (!($image)) {
 
-                [$original_width, $original_height] = getimagesize($file_path);
+                [ $original_width, $original_height ] = getimagesize($file_path);
 
                 if (!($height && $width)) {
 
                     if ($height) {
 
                         $width = ceil($height * $original_width / $original_height);
+
                     } else if ($width) {
 
                         $height = ceil($width * $original_height / $original_width);
+
                     }
+
                 }
 
                 if ($mime === 'image/png') {
@@ -61,10 +66,28 @@ function DPXServeImage(string $file_path, int $width = null, int $height = null,
 
                         imagepng($image, DPXImageServer_Storage . "/$filename");
 
-                        $image = shell_exec("pngquant --quality=" . ($quality - 10) . "-$quality - < " . escapeshellarg(DPXImageServer_Storage . "/$filename"));
-                    } else {
+                        if ($quality < 100) {
 
-                        $image = shell_exec("pngquant --quality=" . ($quality - 10) . "-$quality - < " . escapeshellarg($file_path));
+                            $image = shell_exec("pngquant --quality=" . ($quality - 10) . "-$quality - < " . escapeshellarg(DPXImageServer_Storage . "/$filename"));
+
+                        }else{
+
+                            $image = file_get_contents(DPXImageServer_Storage . "/$filename");
+
+                        }
+
+                    }else{
+
+                        if ($quality < 100) {
+
+                            $image = shell_exec("pngquant --quality=" . ($quality - 10) . "-$quality - < " . escapeshellarg($file_path));
+
+                        }else{
+
+                            $image = file_get_contents($file_path);
+
+                        }
+                        
                     }
 
                     if (!($image)) {
@@ -72,14 +95,19 @@ function DPXServeImage(string $file_path, int $width = null, int $height = null,
                         if (file_exists(DPXImageServer_Storage . "/$filename")) {
 
                             $image = file_get_contents(DPXImageServer_Storage . "/$filename");
-                        } else {
+
+                        }else{
 
                             $image = file_get_contents($file_path);
+
                         }
+
                     }
 
                     @unlink(DPXImageServer_Storage . "/$filename");
-                } else if ($mime === 'image/jpg' || $mime === 'image/jpeg') {
+
+
+                }else if ($mime === 'image/jpg' || $mime === 'image/jpeg') {
 
                     if ($width || $height) {
 
@@ -87,21 +115,26 @@ function DPXServeImage(string $file_path, int $width = null, int $height = null,
                         $original_image = imagecreatefromjpeg($file_path);
                         imagecopyresampled($image, $original_image, 0, 0, 0, 0, $width, $height, $original_width, $original_height);
                         imagedestroy($original_image);
-                    } else {
+
+                    }else{
 
                         $image = imagecreatefromjpeg($file_path);
+
                     }
 
                     ob_start();
-                    imagejpeg($image, null, $quality);
+                    imagejpeg($image, null, $quality < 100 ? $quality : null);
                     $image = ob_get_contents();
                     ob_end_clean();
+
                 }
 
                 if ($cache) {
 
                     file_put_contents(DPXImageServer_Storage . "/$filename", $image);
+
                 }
+
             }
 
             if ($output_image) {
@@ -109,12 +142,17 @@ function DPXServeImage(string $file_path, int $width = null, int $height = null,
                 header("Content-Type: $mime");
                 header("Content-Length: " . strlen($image));
                 die($image);
-            } else {
+
+            }else{
 
                 return $image;
+
             }
+
         }
+       
     }
 
     return false;
+
 }
